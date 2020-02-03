69474ad84eed77f54e5249293a0b08b8d2f19a4f
Josh Sinfield <josh.sinfield@cdl.co.uk>
2020-02-03 15:20:45 +0000

    fix: add parsing of an S3EventRecord to get bucket and key

---
+++ aws-lambda/s3/event.go
@@ -0,0 +1,15 @@
+package sns
+
+import "github.com/aws/aws-lambda-go/events"
+
+func ParseS3EventRecord(record events.S3EventRecord) (sourceBucket, objectKey string) {
+	s3Record := record.S3
+
+	// Retrieve the Bucket Name where the event originated from.
+	sourceBucket = s3Record.Bucket.Name
+
+	// Retrieve object key
+	objectKey = s3Record.Object.Key
+
+	return sourceBucket, objectKey
+}
